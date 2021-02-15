9f65c2404b0cf249fec89a47cc294d29334f1392
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-02-15 11:27:02 +0900

    微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  validates :birthday,            presence: true
+  with_options presence: true do
+    validates :nickname,            presence: true
+    validates :birthday,            presence: true
+  end
 
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナ文字を使用してください' } do
     validates :first_name_kana
