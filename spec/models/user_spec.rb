92f97e0c003c2feb08439336c13b9e06d02886f0
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-02-15 09:40:57 +0900

    重複したemailが存在する場合登録できないことのテスト実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -135,5 +135,13 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is invalid')
     end
+
+    it '重複したemailが存在する場合登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
   end
 end
