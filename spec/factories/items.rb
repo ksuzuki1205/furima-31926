97e2fa12599d17d48b8b09543d438d4572dbd985
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-02-19 14:25:54 +0900

    テスト初期設定 + 商品名が必須であることのテストの実行

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image                  { 'あああああ'}
+    title                  { 'あああああ'}
+    explanation            { 'アイウエオ' }
+    category_id            { 2 }
+    condition_id           { 2 }
+    shipping_charge_id     { 2 }
+    region_id              { 2 }
+    delivery_schedule_id   { 2 }
+    price                  { 20000 }
   end
 end
