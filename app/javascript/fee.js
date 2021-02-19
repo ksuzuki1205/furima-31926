587bf5746eb7438864579645fd120edefe9a7360
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-02-19 10:10:08 +0900

    販売価格機能のjsによる実装

---
+++ app/javascript/fee.js
@@ -0,0 +1,13 @@
+function fee (){
+  const itemPrice  = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+    const salesFee  = document.getElementById("add-tax-price");
+    const salesProfit  = document.getElementById("profit");
+    salesFee.innerHTML = Math.floor(itemPrice.value * 0.1).toLocaleString();
+    // console.log(itemPrice.value)
+    // console.log(salesFee.value)
+    salesProfit.innerHTML = (itemPrice.value - Math.floor(itemPrice.value * 0.1)).toLocaleString();
+  });
+}
+
+window.addEventListener('load', fee);
