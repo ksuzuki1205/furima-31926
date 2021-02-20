f974dfb54fd6a322e008257064015b6dfbb787c5
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-02-20 17:33:10 +0900

    ランダム性の向上によるテスト精度UP

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     # image                  { 'あああああ'}
     title                  { 'あああああ'}
     explanation            { 'アイウエオ' }
-    category_id            { 2 }
-    condition_id           { 2 }
-    shipping_charge_id     { 2 }
-    region_id              { 2 }
-    delivery_schedule_id   { 2 }
+    category_id            { Random.rand(2 .. 11) }
+    condition_id           { Random.rand(2 .. 6) }
+    shipping_charge_id     { Random.rand(2 .. 3) }
+    region_id              { Random.rand(2 .. 48) }
+    delivery_schedule_id   { Random.rand(2 .. 4) }
     price                  { Random.rand(300 .. 9999999)}                   
     user
     after(:build) do |item|
