e1cc43d012243a96ab2972f9edfc5840f60aaa1b
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-03-06 09:37:10 +0900

    マイグレーションファイルの整備

--- db/migrate/20210305063915_create_orders.rb
+++ db/migrate/20210305063915_create_orders.rb
@@ -1,15 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.string  :postal_code,     null: false
-      t.integer :region_id,       null: false
-      t.string  :city,            null: false
       t.string  :block_number,    null: false
-      t.string  :building_name,   null: false
-      t.string  :phone,           null: false
-      t.string  :order
-
-
+      t.string      :postal_code,     null: false
+      t.integer     :region_id,       null: false
+      t.string      :city,            null: false
+      t.string      :block_number,    null: false
+      t.string      :building_name,   null: false
+      t.string      :phone,           null: false
+      # t.references  :order_history,   null: false, foreign_key: true
       t.timestamps
     end
   end
