2a00a18d122e8ea2cac9f595e66325873ea69faf
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-03-06 16:49:36 +0900

    orderモデルにバリデーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
-  
-
+  with_options presence: true do
+    validates  :postal_code
+    validates  :region_id
+    validates  :city
+    validates  :block_number
+    validates  :phone
+  end
 end
