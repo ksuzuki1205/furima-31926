b91622744e0dbd2a79b3b1296dbc5dbc92cc75cf
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-03-06 17:51:28 +0900

    コントローラーの仮設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+  def index
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @user.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def item_params
+    params.require(:order).permit(:postal_code, :region_id, :city, :block_number, :phone, :region_id, :price,
+                                 :delivery_schedule_id, :image).merge(order_history_id: params[:order_history_id])
+  end
+
 end
