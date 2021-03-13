fb74fcbbc44a6c8717db4107cc4043ad022b3573
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-03-13 11:50:35 +0900

    バリデーションの設定

--- app/models/order_connection.rb
+++ app/models/order_connection.rb
@@ -3,15 +3,21 @@ class OrderConnection
   attr_accessor :postal_code, :region_id, :city, :block_number, :building_name, :phone, :item_id, :user_id
 
   with_options presence: true do
-    validates :postal_code
-    validates :region_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city
     validates :block_number
-    validates :phone
+    validates :phone, format: { with: /\A\d{11}\z/, message: "is invalid. Input half-width number only" }
   end
 
+  # numericality: { with: /^0\d{11}$/, message: "is invalid. Input half-width number only"}
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :region_id
+  end
+
+
   def save
-      order_history = OrderHistory.create(item_id: item_id, user_id: user_id)
+    order_history = OrderHistory.create(item_id: item_id, user_id: user_id)
     Order.create(postal_code: postal_code, region_id: region_id, city: city, block_number: block_number, building_name: building_name, phone: phone, order_history_id: order_history.id)
   end
 end
