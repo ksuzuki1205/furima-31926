d78c064ce9eef26442931e8d2b95e259d84c93a4
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-03-13 22:33:06 +0900

    フォームの情報の取得
    フォームの情報ほ取得は怪しい

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,18 @@
 const pay = () => {
   const form = document.getElementById("charge-form");
-  Payjp.setPublicKey("pk_test_265374d86ede432d93d074ce"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey("pk_test_265374d86ede432d93d074ce"); 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    // console.log("フォーム送信時にイベント発火")
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_connection[hoge]"),
+      cvc: formData.get("order_connection[hoge]"),
+      exp_month: formData.get("order_connection[hoge]"),
+      exp_year: `20${formData.get("order_connection[hoge]")}`,
+    };
   });
 };
 
