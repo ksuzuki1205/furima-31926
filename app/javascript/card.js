d39738727e22eac870d58822e5f870d30410f9e4
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-03-13 17:20:36 +0900

    公開鍵を設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
   const form = document.getElementById("charge-form");
+  Payjp.setPublicKey("pk_test_265374d86ede432d93d074ce"); // PAY.JPテスト公開鍵
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("フォーム送信時にイベント発火")
