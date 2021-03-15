e0eae153a86c22a53c059de497b4942ee01c7b04
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-03-15 13:50:56 +0900

    ビューの整備とfactory.botの作成

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order do
-    
+    postal_code            { '123-4567' }
+    region_id              { Random.rand(2..48) }
+    city                   { 'ああああああああ'} 
+    block_number           { 'いいいいいいいい'}
+    building_name          { 'うううううううう'}
+    phone                  { '08012345678' }
+    order_history
   end
 end
