e86ac38c21ecd322884c5e38d7139fdfd5cae096
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-04-05 09:36:05 +0900

    単独画像への仕様変更

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_003502) do
     t.integer "region_id", null: false
     t.string "city", null: false
     t.string "block_number", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
