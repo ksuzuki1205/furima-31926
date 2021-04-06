7743ba13d7cb345a50ba0797752629d6d24affe1
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-04-06 14:02:46 +0900

    日本語化の実装（テスト未実施）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources :users, only: [:show, :update]
   root to: 'items#index'
   resources :items do
     resources :orders, only: [:index, :create]
